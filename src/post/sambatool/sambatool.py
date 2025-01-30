7fa2c8a0ac95b3a164ede9c3ae58fbd44103db8e
Mohammad NIAEI <m.shemuni@gmail.com>
2025-01-30 11:47:33 +0300

    sambatool class added

--- src/post/sambatool/sambatool.py
+++ src/post/sambatool/sambatool.py
@@ -1,10 +1,13 @@
 from logging import Logger
 from typing import Optional
 
+from typing_extensions import Self
+
 from .gpo import GPO
 from .user import User
 from post.connection.model_connector import ModelConnector
 from post.utils.common import GLOBAL_LOGGER
+from .. import SSHConnector
 
 
 class SambaTool:
@@ -27,3 +30,28 @@ class SambaTool:
 
     def __repr__(self) -> str:
         return self.__str__()
+
+    @classmethod
+    def from_ssh_connector(cls, address: str, port: int, user: str, passwd: str,
+                           ad_passwd: Optional[str] = None, sudo_passwd: Optional[str] = None,
+                           logger: Optional[Logger] = None) -> Self:
+        """
+        Constructs a GPO object using an ssh connection information.
+
+        Args:
+            address (str): An IP address or hostname.
+            port (int): The ssh port. Most probably is 22.
+            user (str): The ssh username.
+            passwd (str): The ssh user's password.
+            ad_passwd (str): password of the administrator of AD
+            sudo_passwd (str): sudo password
+            logger (Logger, optional): A logger to log. Defaults to None.
+
+        Returns:
+            Self: A GPO object.
+
+        Raises:
+            ValueError: If a connection cannot be established
+        """
+        ssh_connector = SSHConnector(address, port, user, passwd, logger=logger)
+        return cls(ssh_connector, ad_passwd=ad_passwd, sudo_passwd=sudo_passwd, logger=logger)
